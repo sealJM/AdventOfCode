183a4f396ab7ee51c8b83a041fe69738b098f78c
sealJM <bradley_macd@live.com>
2023-12-19 01:10:21 -0500

    Note

--- 2023/Day4-ScratchCards/Part2/recursive.py
+++ 2023/Day4-ScratchCards/Part2/recursive.py
@@ -15,7 +15,8 @@ with open(file_path, 'r') as file:
 def process_line(index=0, end=len(lines)):
     global results
     global lines
-    # This attempt was just for fun and doesn't work for now, its somewhat off
+    # This attempt was just for fun and doesn't work for now
+    # It's also REALLY slow
     for i in range(index, end):
         wins = 0
         if i >= len(lines):
