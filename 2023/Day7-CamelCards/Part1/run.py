13d0a477ec7626272f41969f34ec039576027756
sealJM <bradley_macd@live.com>
2023-12-21 18:23:24 -0500

    Solved

--- 2023/Day7-CamelCards/Part1/run.py
+++ 2023/Day7-CamelCards/Part1/run.py
@@ -1,14 +1,36 @@
-# import concurrent.futures
 import timeit
 
 
 def process_line(line):
-    return line
+    power = {
+        "2": 2, "3": 3, "4": 4, "5": 5, "6": 6,
+        "7": 7, "8": 8, "9": 9, "T": 10, "J": 11,
+        "Q": 12, "K": 13, "A": 14
+    }
+    hand, bet = line.split()
+    char_count = {}
+    pairs = 0
+    cards = []
+    for char in hand:
+        cards.append(power[char])
+        char_count[char] = char_count.get(char, 0) + 1
+    for i in char_count:
+        if char_count[i] == 2:
+            pairs += 1
+        elif char_count[i] == 3:
+            pairs += 3
+        elif char_count[i] == 4:
+            pairs += 5
+        elif char_count[i] == 5:
+            pairs += 6
+    power = [pairs] + cards
+    return [power, bet]
 
 
 def run():
-    global results
-    results = 0
+    global total
+    total = 0
+    results = []
 
     # Specify the file path
     file_path = '2023\\Day7-CamelCards\\input.txt'
@@ -18,11 +40,14 @@ def run():
         # Read lines and store them in a list
         lines = [line.rstrip('\n') for line in file]
 
-    # for line in lines:
-    results = process_line(lines)
+    for line in lines:
+        results.append(process_line(line))
+    results = sorted(results, key=lambda x: x[0])
+    for i, rank in enumerate(results):
+        total += int(rank[1])*(i+1)
 
 
 if __name__ == "__main__":
     execution_time = (timeit.timeit(run, number=1000))/1000
-    print(results)
+    print(total)
     print(f"Execution time: {execution_time} seconds")
